9d424af84ec3177f70654de02c172d028c165edb
qqri <50269335+qqri@users.noreply.github.com>
2020-12-18 18:57:55 +0900

    fix

---
+++ src/main/java/com/krkimb/authproject2/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.krkimb.authproject2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
